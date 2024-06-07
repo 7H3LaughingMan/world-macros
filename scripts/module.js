b3b12e032884754d7e1098825df24a6bbd5f55ff
7H3LaughingMan <7H3LaughingMan@proton.me>
2024-06-07 21:57:29 +0000

    Implemented running macros in the World Macros folder.

---
+++ scripts/module.js
@@ -0,0 +1,21 @@
+function executeMacros(folder) {
+    for (const macro of folder.contents) {
+        try {
+            macro.execute();
+            console.debug(`World Macros | Executed "${macro.name}" (ID: ${macro.id})`);
+        } catch (err) {
+            console.error(`World Macros | Error Executing "${macro.name}" (ID: ${macro.id})`, err);
+        }
+    }
+
+    for (const subfolder of folder.getSubfolders()) {
+        executeMacros(subfolder);
+    }
+}
+
+Hooks.once("ready", () => {
+    const folder = game.macros.folders.find(f => f.name == "World Macros" && f.depth == 1);
+    if(typeof folder !== "undefined") {
+        executeMacros(folder);
+    }
+});
